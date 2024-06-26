42e1ca68a201ce2c5148e006bc774c2e2284b4d0
francis DAVID <contato@fdavid.com.br>
2024-06-26 07:16:09 -0300

    hotfix: param host and default thing

--- publisher.sh
+++ publisher.sh
@@ -2,17 +2,29 @@
 #!/bin/bash
 source .env
 
-THING=$1
+HOST=$1
+THING=$2
+SENSOR=$3
 
 PSQL="psql -h $POSTGRES_HOST -U $POSTGRES_USER -d $POSTGRES_DB"
+
+if [ -n "$HOST" ]; then
+    MOSQUITTO_HOST=$HOST
+fi
+
 PUB="mosquitto_pub -i "scriptPublisher" -h $MOSQUITTO_HOST -u $MOSQUITTO_USER -P $MOSQUITTO_PASSWORD -q $MOSQUITTO_QOS"
 
 if [ -z "$THING" ]; then
-    export THING=1
+    THING=$($PSQL -c "select id from things limit 1" -t)
 fi
 
 UUID=$($PSQL -c "select uuid from things where id = $THING" -t)
-THINGSENSOR=$($PSQL -c "select id from thingssensors where id_thing = $THING limit 1" -t)
+
+if [ -z "$SENSOR" ]; then
+    THINGSENSOR=$($PSQL -c "select id_sensor from thingssensors where id_thing = $THING limit 1" -t)
+else
+    THINGSENSOR=$($PSQL -c "select id_sensor from thingssensors where id_thing = $THING and id_sensor = $SENSOR" -t)
+fi
 
 if [ -z "$UUID" ]; then
     echo -e "Thing ID $THING do not found"
@@ -31,6 +43,7 @@ PAYLOAD="{\"dt\": \"$DATE\", \"value\": $VALUE}"
 TOPIC=$UUID/$THINGSENSOR
 TOPIC=$(echo $TOPIC | sed 's/ //g') #Remove whitespace
 
+echo -e "Host:\t$MOSQUITTO_HOST"
 echo -e "Thing:\t$UUID"
 echo -e "Sensor:\t$THINGSENSOR"
 echo -e "Topic:\t$TOPIC"
